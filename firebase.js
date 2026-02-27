5aff5ed020b2560f80ff272568de8a1ccad86104
rammsdoct <rammsdoct@gmail.com>
2026-02-26 20:37:05 -0600

    Config Firebase Google Sign-In (Web Client ID)

--- firebase.js
+++ firebase.js
@@ -1,21 +1,18 @@
 import { initializeApp } from "firebase/app";
-import {
-  initializeAuth,
-  getReactNativePersistence
-} from "firebase/auth";
-import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCJ-T_cwN6F-8SVOB48fIqqbTJSDtSoGz8",
   authDomain: "presawatch.firebaseapp.com",
   projectId: "presawatch",
-  storageBucket: "presawatch.firebasestorage.app",
+  storageBucket: "presawatch.appspot.com",
   messagingSenderId: "344850391677",
-  appId: "1:344850391677:web:be62d08cb1fc8e68ce4714"
+  appId: "1:344850391677:web:be62d08cb1fc8e68ce4714",
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
-});
+// ✅ Auth y Firestore estándar (recomendado en Expo)
+export const auth = getAuth(app);
+export const db = getFirestore(app)
